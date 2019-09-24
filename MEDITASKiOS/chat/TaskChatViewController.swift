e6a40adc03b1789e61325d3d784ea2c706360239
= <=>
2019-09-23 20:11:43 -0500

    chat view controller hooked up in storyboard

---
+++ MEDITASKiOS/chat/TaskChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskChatViewController.swift
+//  MEDITASKiOS
+//
+//  Created by Mark Robinson on 9/23/19.
+//  Copyright © 2019 MohammedRaheem. All rights reserved.
+//
+
+import UIKit
+
+class TaskChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
