c7ebb92668d0cc191b84eb6f7f020357f910d727
= <=>
2019-11-21 20:40:14 -0600

    join team gui and table view integrated with nav bar

--- MEDITASKiOS/team/JoinTeamTableViewController.swift
+++ MEDITASKiOS/team/JoinTeamTableViewController.swift
@@ -16,8 +16,44 @@ class JoinTeamTableViewController : UITableViewController {
     
     let currentUId = Auth.auth().currentUser!.uid
     
+    @IBAction func cancelAction(_ sender: Any) {
+        closeThisView();
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        closeThisView();
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        print("yo 0 /myarray.count");
+        
+        return myarray.count;
+    }
+    
+    func closeThisView() {
+        navigationController?.popViewController(animated: true)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "joinTeamTableCell", for: indexPath)
+        
+        //let notification = notificationStore.getNotification(index: indexPath.item)
+        print("yo 1");
+        
+        print("/myarray[indexPath.item]");
+        
+        cell.textLabel?.text = myarray[indexPath.item];
+        //cell.notificationId = notification.notifyId
+        //cell.notificationLabel.text = notifyList[indexPath.item].msg
+        return cell
+    }
+    
     override func viewDidLoad() {
-        myarray = ["item1", "item2", "item3"]
+        myarray = ["Team 1", "Team 2", "Team 3"];
         
         //joinTeamTable.beginUpdates()
         //joinTeamTable.insertRowsAtIndexPaths([
